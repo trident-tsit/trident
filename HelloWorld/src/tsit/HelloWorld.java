6154905605d12868df3b2d8d70243782376ede70
trident <trident@172.16.28.121>
2018-07-12 10:08:02 +0900

    1回目

---
+++ HelloWorld/src/tsit/HelloWorld.java
@@ -0,0 +1,13 @@
+package tsit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Hello");
+		System.out.println("Hello Hello");
+		System.out.println("Hello Hello");
+		System.out.println("Hello Hello");
+	}
+
+}
